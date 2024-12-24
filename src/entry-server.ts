c467c24273814a767cd471f17b61414f85861e67
Dino <dnp.digital@gmail.com>
2024-12-24 12:34:36 +0200

    cleanup

--- src/entry-server.ts
+++ src/entry-server.ts
@@ -1,18 +1,11 @@
 // entry-server.ts
-// import env from "vanjs";
-// import env from "vanjs";
-// import van from "vanjs-core";
 import { renderPreloadLinks } from "./server-util/renderPreloadLinks.ts";
 import { renderToHTML } from "./server-util/renderToHTML.ts";
 
 import App from "./app.ts";
 
 export async function render(_url: string, manifest: Record<string, string[]>) {
-  // console.log('entry-server.time --------------------------->', new Date().toISOString(), env);
-  // console.log('entry-server.time --------------------------->', new Date().toISOString(), { van, vanX });
-  // const { van } = env;
   const head = '';
-  // const html = renderToHTML(Fragment(App()));
   const html = renderToHTML(App());
   const preloadLinks = renderPreloadLinks(Object.keys(manifest), manifest);
   return { html, head, preloadLinks };
